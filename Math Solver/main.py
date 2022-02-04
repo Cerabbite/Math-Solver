8f438db007942b5a07fa7f670da431423cafed42
KevinGiesberts <kevin.giesberts@gmail.com>
2022-02-04 10:54:40 +0100

    Update main.py

--- Math Solver/main.py
+++ Math Solver/main.py
@@ -16,14 +16,19 @@ class main:
         self.inp_box.pack()
         self.inp_box.insert('end', default_values.default_equation())
 
+        self.bindings()
 
-    def update(self):
+    def bindings(self):
+        window.bind("<Key>", lambda x : self.update(x))
+
+
+    def update(self, x):
         #value = self.inp_box.get("1.0", END)
         #print(value)
         print('x')
 
 if __name__ == "__main__":
     window = Tk()
-    main(window)
-    window.bind("<Up>", main.update())
+    f_main = main(window)
+    #window.bind("<Up>", f_main.update())
     window.mainloop()
