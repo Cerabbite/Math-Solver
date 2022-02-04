7d29e17c9af08516b6f235faa0d6a9c86bb2445f
KevinGiesberts <kevin.giesberts@gmail.com>
2022-02-04 11:01:47 +0100

    Update main.py

--- Math Solver/main.py
+++ Math Solver/main.py
@@ -11,7 +11,7 @@ class main:
         self.window = window
         self.window.title("Math solver")
 
-        self.inp_box_width = 11
+        self.inp_box_width = len(default_values.default_equation())
         self.inp_box = Text(self.window, width=self.inp_box_width, height=1)
         self.inp_box.pack()
         self.inp_box.insert('end', default_values.default_equation())
@@ -23,9 +23,12 @@ class main:
 
 
     def update(self, x):
-        #value = self.inp_box.get("1.0", END)
-        #print(value)
-        print('x')
+        self.value = self.inp_box.get("1.0", END)
+        self.lvalue = len(self.value)
+        if self.lvalue < 12: self.lvalue = 12
+        self.inp_box_width = self.lvalue
+        self.inp_box.pack()
+        #print(len(value))
 
 if __name__ == "__main__":
     window = Tk()
