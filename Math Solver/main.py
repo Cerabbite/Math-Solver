54c6b2909ac949429bceca9526cb3bf054769263
KevinGiesberts <kevin.giesberts@gmail.com>
2022-02-04 11:16:52 +0100

    Update main.py

--- Math Solver/main.py
+++ Math Solver/main.py
@@ -11,10 +11,21 @@ class main:
         self.window = window
         self.window.title("Math solver")
 
+        self.lbl = Label(self.window, text="Equation")
+        self.lbl.grid(row=0,column=0)
+
         self.inp_box_width = 25#len(default_values.default_equation())
         self.inp_box = Text(self.window, width=self.inp_box_width, height=1)
-        self.inp_box.pack()
+        self.inp_box.grid(row=0, column=1)
         self.inp_box.insert('end', default_values.default_equation())
+
+        self.lbl2 = Label(self.window, text="Solve for")
+        self.lbl2.grid(row=1,column=0)
+
+        self.inp_box_width2 = 25#len(default_values.default_equation())
+        self.inp_box2 = Text(self.window, width=self.inp_box_width2, height=1)
+        self.inp_box2.grid(row=1, column=1)
+        #self.inp_box.insert('end', default_values.default_equation())
 
         #self.bindings()
 
