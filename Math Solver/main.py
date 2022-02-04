f03b1b58c1cbde70b0c8f5a8910fdb64b138167b
KevinGiesberts <kevin.giesberts@gmail.com>
2022-02-04 17:09:58 +0100

    Update main.py

--- Math Solver/main.py
+++ Math Solver/main.py
@@ -5,58 +5,66 @@ class solver:
     def __init__(self):
         pass
 
-    def break_equation(self):
-        pass
+    def equation_components(self, equation):
+        components = {"Letters": [], "Numbers": [], "Special chars": []}
+        for i in equation:
+            if i.isdigit():
+                components['Numbers'].append(int(i))
+            elif i.isalnum():
+                components['Letters'].append(str(i))
+            else:
+                components['Special chars'].append(str(i))
+        return components
 
 
 class main:
     def __init__(self, window):
         self.window = window
         self.window.title("Math solver")
+        self.letters = []
+        self.letters_btns = []
+        Solver = solver()
+        self.eq_comps = Solver.equation_components(default_values.default_equation())
+        self.create_btn()
 
         self.equation_lbl = Label(self.window, text="Equation")
         self.equation_lbl.grid(row=0,column=0)
 
         self.inp_box_width = 25#len(default_values.default_equation())
         self.inp_box = Text(self.window, width=self.inp_box_width, height=1)
-        self.inp_box.grid(row=0, column=1)
+        self.inp_box.grid(row=0, column=1, columnspan=2)
         self.inp_box.insert('end', default_values.default_equation())
 
         self.solve_for_lbl = Label(self.window, text="Solve for")
         self.solve_for_lbl.grid(row=1,column=0)
 
-        self.solve_for_box_width = 25#len(default_values.default_equation())
-        self.solve_for_box = Text(self.window, width=self.solve_for_box_width, height=1)
-        self.solve_for_box.grid(row=1, column=1)
-
         self.solve_btn_width = 20
         self.solve_btn = Button(self.window, width=self.solve_btn_width, text="Solve", command=self.solve_eq_data)
-        self.solve_btn.grid(row=2, column=0, columnspan=2)
+        self.solve_btn.grid(row=2, column=0, columnspan=3)
         #self.inp_box.insert('end', default_values.default_equation())
 
         #self.bindings()
 
     def bindings(self):
-        #window.bind("<enter>", lambda x : self.solve_eq_data(x))
+        #window.bind("<Key>", lambda x : self.update(x))
+        pass
+
+    def create_btn(self):
+        print(self.eq_comps['Letters'])
+        for i in self.eq_comps['Letters']:
+            if not i in self.letters:
+                self.letters.append(i)
+                btn = Button(self.window, width=5, text=i)
+                btn.grid(row=1, column=1)
+                self.letters_btns.append(btn)
+                print(btn)
+
+    def on_off(self):
         pass
 
 
     def solve_eq_data(self):
-        self.solve_for = self.solve_for_box.get("1.0", END)
-        print(self.solve_for)
-        self.solve_for.replace(" ", "")
-        if self.solve_for_box.compare("end-1c", "==", "1.0"):
-            messagebox.showerror("Empty", "Please enter one character")
-            return
-        if len(self.solve_for_box.get("1.0", 'end-1c')) > 1:
-            messagebox.showerror("To many characters", "Please enter just one character")
-            return
-
-        self.equation = self.inp_box.get("1.0", END)
-        self.equation_copy = filter(str.isdigit, self.equation)
-        self.equation_copy = "". join(self.equation_copy)
-        print(self.equation_copy)
-        #messagebox.showerror("Character not found", "Please enter a character from the equation")
+        pass
 
     """def update(self, x):
         self.value = self.inp_box.get("1.0", END)
