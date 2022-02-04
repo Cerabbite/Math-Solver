62c5fc4559e36384d613acf0503d86545882697c
KevinGiesberts <kevin.giesberts@gmail.com>
2022-02-04 10:19:12 +0100

    Update main.py

--- Math Solver/main.py
+++ Math Solver/main.py
@@ -1,9 +1,18 @@
 from libaries import *
 
 
+class solver:
+    def __init__(self):
+        pass
+
+
 class main:
     def __init__(self, window):
         self.window = window
+        self.window.title("Math solver")
+
+        self.inp_box = Text(self.window, width=10)
+        self.inp_box.pack()
 
 if __name__ == "__main__":
     window = Tk()
